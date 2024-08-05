2c984c1c69c19e2032c25cbc60579a5670db4a25
theinternauts <cricketw@WeAreTheInternauts.com>
2024-08-05 10:26:07 +0900

    Initial Commit

---
+++ TestingExternalSDKBinary/TestingExternalSDKBinary.h
@@ -0,0 +1,18 @@
+//
+//  TestingExternalSDKBinary.h
+//  TestingExternalSDKBinary
+//
+//  Created by Cricket on 8/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestingExternalSDKBinary.
+FOUNDATION_EXPORT double TestingExternalSDKBinaryVersionNumber;
+
+//! Project version string for TestingExternalSDKBinary.
+FOUNDATION_EXPORT const unsigned char TestingExternalSDKBinaryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestingExternalSDKBinary/PublicHeader.h>
+
+
